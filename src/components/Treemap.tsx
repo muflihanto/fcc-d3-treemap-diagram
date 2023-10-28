90fdc2ab8d53ed4114f0037276b85a4f4a72530b
muflihanto <muflihanto@gmail.com>
2023-10-28 19:23:42 +0700

    Add legend

--- src/components/Treemap.tsx
+++ src/components/Treemap.tsx
@@ -16,12 +16,11 @@ export interface DataCollection {
 }
 
 const colors = ["rgb(53,97,143)", "rgb(161,216,50)", "rgb(250,121,245)", "rgb(26,213,74)", "rgb(222,43,113)", "rgb(103,218,188)", "rgb(117,46,79)", "rgb(105,144,35)", "rgb(161,19,178)", "rgb(252,209,7)", "rgb(81,62,180)", "rgb(208,217,174)", "rgb(37,36,249)", "rgb(253,143,32)", "rgb(129,132,251)", "rgb(132,84,26)", "rgb(117,179,216)", "rgb(11,83,19)", "rgb(243,197,250)", "rgb(214,6,26)"];
+const color = d3.scaleOrdinal<string>().range(colors.map((color) => d3.interpolateRgb(color, "#fff")(0.2)));
 
 export default function Treemap({ data }: { data: unknown }) {
   const width = 960;
   const height = 600;
-
-  const color = d3.scaleOrdinal<string>().range(colors.map((color) => d3.interpolateRgb(color, "#fff")(0.2)));
   const treemap = d3.treemap<Data>().size([width, height]).paddingInner(1)(
     d3
       .hierarchy<Data>(data as Data)
@@ -31,48 +30,100 @@ export default function Treemap({ data }: { data: unknown }) {
   );
 
   return (
-    <div className="flex flex-col items-center mt-8 overflow-scroll max-w-full">
-      <svg
-        id="tree-map"
-        width={width}
-        height={height}
-        className="self-start lg:self-center"
-      >
-        {treemap.leaves().map((d) => {
+    <>
+      <div className="flex flex-col items-center mt-8 overflow-scroll max-w-full lg:p-0 p-8">
+        <svg
+          id="tree-map"
+          width={width}
+          height={height}
+          className="self-start lg:self-center"
+        >
+          {treemap.leaves().map((d) => {
+            return (
+              <g
+                className="group"
+                key={d.data.id}
+                transform={`translate(${d.x0},${d.y0})`}
+              >
+                <rect
+                  id={d.data.id}
+                  className="tile"
+                  width={d.x1 - d.x0}
+                  height={d.y1 - d.y0}
+                  data-name={d.data.name}
+                  data-category={d.data.category}
+                  data-value={d.data.value}
+                  fill={color(d.data.category)}
+                />
+                <text className="tile-text text-[10px]">
+                  {d.data.name.split(" ").map((name, idx) => {
+                    const ratios = ["#000", "#FFF"].map((c) => contrast.ratio(c, color(d.data.category)));
+                    return (
+                      <tspan
+                        key={`${name}-${idx}`}
+                        x={4}
+                        y={12 + idx * 12}
+                        fill={ratios[0] > ratios[1] ? "#000" : "#FFF"}
+                      >
+                        {name}
+                      </tspan>
+                    );
+                  })}
+                </text>
+              </g>
+            );
+          })}
+        </svg>
+      </div>
+      <div className="flex flex-col items-center overflow-scroll max-w-full sm:p-0 p-4">
+        <Legend treemap={treemap} />
+      </div>
+    </>
+  );
+}
+
+function Legend({ treemap }: { treemap: d3.HierarchyRectangularNode<Data> }) {
+  const width = 500;
+  const categories = treemap
+    .leaves()
+    .map((d) => d.data.category)
+    .filter((el, idx, arr) => arr.indexOf(el) === idx);
+
+  const RECT_SIZE = 15;
+  const H_SPACING = 150;
+  const V_SPACING = 10;
+  const elemsPerRow = Math.floor(width / H_SPACING);
+
+  return (
+    <svg
+      id="legend"
+      className="mt-4 self-start sm:self-center text-[14px]"
+      width={width}
+    >
+      <g transform={"translate(60,10)"}>
+        {categories.map((d, i) => {
+          const [x, y] = [(i % elemsPerRow) * H_SPACING, Math.floor(i / elemsPerRow) * RECT_SIZE + V_SPACING * Math.floor(i / elemsPerRow)];
           return (
             <g
-              className="group"
-              key={d.data.id}
-              transform={`translate(${d.x0},${d.y0})`}
+              key={d}
+              transform={`translate(${x},${y})`}
             >
               <rect
-                id={d.data.id}
-                className="tile"
-                width={d.x1 - d.x0}
-                height={d.y1 - d.y0}
-                data-name={d.data.name}
-                data-category={d.data.category}
-                data-value={d.data.value}
-                fill={color(d.data.category)}
+                width={RECT_SIZE}
+                height={RECT_SIZE}
+                className="legend-item"
+                fill={color(d)}
               />
-              <text className="tile-text text-[10px]">
-                {d.data.name.split(" ").map((name, idx) => {
-                  return (
-                    <tspan
-                      key={`${name}-${idx}`}
-                      x={4}
-                      y={12 + idx * 12}
-                      fill={contrast.ratio("#000", color(d.data.category)) > 10 ? "#000" : "#FFF"}
-                    >
-                      {name}
-                    </tspan>
-                  );
-                })}
+              <text
+                x={RECT_SIZE + 3}
+                y={RECT_SIZE + -2}
+              >
+                {d}
               </text>
             </g>
           );
         })}
-      </svg>
-    </div>
+      </g>
+    </svg>
   );
 }
